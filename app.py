ddcf4536ab775a6b626f64e6ce07c5aa4c4978db
Hamza <bakkar.hamza92@gmail.com>
2023-11-01 13:22:16 +0100

    Creat Db User with Login and register html

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
 from flask import Flask, request, Response, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 import requests
  
 app = Flask(__name__)
 
-@app.route('/favicon.ico')
-def favicon():
-    return Response('', status=200)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    password_confirm = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, username, email, password, password_confirm):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.password_confirm = password_confirm
 
 @app.route("/")
 def accueil():
     exchange_rates = crypto_portfolio()
     return render_template("index.html", exchange_rates=exchange_rates)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 
 api_key = "CA89529B-FA3C-44B1-B65D-3BED3D6AAE70"
 
